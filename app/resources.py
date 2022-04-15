f739214cb54fb39000a7031b1976a224b1847ef0
Jessi Jassal <jessica_jassal@harvard.edu>
2022-04-15 18:18:19 -0400

    Add health checks for DIMS and DTS

--- app/resources.py
+++ app/resources.py
@@ -1,9 +1,7 @@
+import requests
 from flask_restx import Resource, Api
-from flask import request, current_app, make_response, jsonify, render_template
+from flask import render_template
 import os, json
-
-# Import custom class files here
-#from . import <class> <class>
 
 def define_resources(app):
     api = Api(app, version='1.0', title='HDC Integration Tests', description='This project contains the integration tests for the HDC project')
@@ -18,3 +16,27 @@ def define_resources(app):
     @app.route('/hello-world')
     def hello_world():
         return render_template('index.html')
+
+    @app.route('/integration/health')
+    def healthcheck_dims():
+        num_failed_tests = 0
+        tests_failed = []
+        result = {"num_failed": num_failed_tests, "tests_failed": tests_failed}
+
+        # Health Check Tests for DIMS
+        health = requests.get('https://ltsds-cloud-dev-1.lib.harvard.edu:10580/health', verify=False)
+        if health.status_code != 200:
+            result["num_failed"] += 1
+            result["tests_failed"].append("DIMS healthcheck")
+            result["DIMS"] = {"status_code": health.status_code, "text": health.text}
+
+        # Health Check Tests for DTS
+        # TODO: status_code for DTS is 404, although the health check is successful
+        health = requests.get('https://ltsds-cloud-dev-1.lib.harvard.edu:10581/healthcheck', verify=False)
+        json_health = json.loads(health.text)
+        if json_health["status"] != "success":
+            result["num_failed"] += 1
+            result["tests_failed"].append("DTS healthcheck")
+            result["DTS"] = {"status_code": health.status_code, "text": health.text}
+
+        return json.dumps(result)
