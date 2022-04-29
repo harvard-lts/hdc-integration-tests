57c90d3fb4b4849680c38a2320a7c5c25e688d69
Jessi Jassal <jessica_jassal@harvard.edu>
2022-04-29 16:27:29 -0400

    Add wait for publish, remove dataverse creation

--- app/resources.py
+++ app/resources.py
@@ -1,4 +1,6 @@
-import requests
+import time
+
+import requests, datetime
 from flask_restx import Resource, Api
 from flask import render_template
 import os, json
@@ -45,121 +47,23 @@ def define_resources(app):
     def dims_ingest_dv():
         num_failed_tests = 0
         tests_failed = []
-        result = {"num_failed": num_failed_tests, "tests_failed": tests_failed}
+        result = {"num_failed": num_failed_tests, "tests_failed": tests_failed, "info": {}}
         dataverse_endpoint = os.environ.get('DATAVERSE_ENDPOINT')
         admin_user_token = os.environ.get('ADMIN_USER_API_TOKEN')
 
-        random_dataverse = {
-          "name": "Scientific Research",
-          "alias": "science",
-          "dataverseContacts": [
-            {
-              "contactEmail": "pi@example.edu"
-            },
-            {
-              "contactEmail": "student@example.edu"
-            }
-          ],
-          "affiliation": "Scientific Research University",
-          "description": "We do all the science.",
-          "dataverseType": "LABORATORY"
-        }
+        app.logger.debug("Loading dataset dictionary")
+        with open('/home/appuser/test_data/dataset-finch1.json') as dataset:
+            data = dataset.read()
 
-        random_dataset = {
-          "datasetVersion": {
-            "metadataBlocks": {
-              "citation": {
-                "fields": [
-                  {
-                    "value": "Darwin's Finches",
-                    "typeClass": "primitive",
-                    "multiple": False,
-                    "typeName": "title"
-                  },
-                  {
-                    "value": [
-                      {
-                        "authorName": {
-                          "value": "Finch, Fiona",
-                          "typeClass": "primitive",
-                          "multiple": False,
-                          "typeName": "authorName"
-                        },
-                        "authorAffiliation": {
-                          "value": "Birds Inc.",
-                          "typeClass": "primitive",
-                          "multiple": False,
-                          "typeName": "authorAffiliation"
-                        }
-                      }
-                    ],
-                    "typeClass": "compound",
-                    "multiple": True,
-                    "typeName": "author"
-                  },
-                  {
-                    "value": [
-                        { "datasetContactEmail" : {
-                            "typeClass": "primitive",
-                            "multiple": False,
-                            "typeName": "datasetContactEmail",
-                            "value" : "finch@mailinator.com"
-                        },
-                        "datasetContactName" : {
-                            "typeClass": "primitive",
-                            "multiple": False,
-                            "typeName": "datasetContactName",
-                            "value": "Finch, Fiona"
-                        }
-                    }],
-                    "typeClass": "compound",
-                    "multiple": True,
-                    "typeName": "datasetContact"
-                  },
-                  {
-                    "value": [ {
-                       "dsDescriptionValue":{
-                        "value":   "Darwin's finches (also known as the Galápagos finches) are a group of about fifteen species of passerine birds.",
-                        "multiple":False,
-                       "typeClass": "primitive",
-                       "typeName": "dsDescriptionValue"
-                    }}],
-                    "typeClass": "compound",
-                    "multiple": True,
-                    "typeName": "dsDescription"
-                  },
-                  {
-                    "value": [
-                      "Medicine, Health and Life Sciences"
-                    ],
-                    "typeClass": "controlledVocabulary",
-                    "multiple": True,
-                    "typeName": "subject"
-                  }
-                ],
-                "displayName": "Citation Metadata"
-              }
-            }
-          }
-        }
+        # reconstructing the data as a dictionary
+        random_dataset = json.loads(data)
 
         headers = {"X-Dataverse-key": admin_user_token}
 
-        # Create Dataverse
-        create_dataverse = requests.post(
-            dataverse_endpoint + '/api/dataverses/root',
-            json=random_dataverse,
-            headers=headers,
-            verify=False)
-        json_create_dv = create_dataverse.json()
-        if json_create_dv["status"] != "OK":
-            result["num_failed"] += 1
-            result["tests_failed"].append("Create Dataverse")
-            result["Create Dataverse"] = {"status_code": create_dataverse.status_code, "text": json_create_dv["message"]}
-
+        app.logger.debug("Creating dataset")
         # Create Dataset
         create_dataset = requests.post(
-            dataverse_endpoint + '/api/dataverses/science/datasets',
+            dataverse_endpoint + '/api/dataverses/archived/datasets',
             json=random_dataset,
             headers=headers,
             verify=False)
@@ -167,36 +71,49 @@ def define_resources(app):
         if json_create_dataset["status"] != "OK":
             result["num_failed"] += 1
             result["tests_failed"].append("Create Dataset")
-            result["Create Dataset"] = {"status_code": create_dataset.status_code, "text": json_create_dataset["message"]}
+            result["Failed Create Dataset"] = {"status_code": create_dataset.status_code, "text": json_create_dataset["message"]}
         dataset_id = json_create_dataset["data"]["id"]
         persistent_id = json_create_dataset["data"]["persistentId"]
+        result["info"]["persistentId"] = persistent_id
+        result["info"]["datasetId"] = dataset_id
+        result["info"]["Create Dataset"] = {"status_code": create_dataset.status_code}
 
-        # Publish Dataverse
-        publish_dataverse = requests.post(
-            dataverse_endpoint
-            + '/api/dataverses/science/actions/:publish',
-            headers=headers,
-            verify=False)
-        json_publish_dv = publish_dataverse.json()
-        if json_publish_dv["status"] != "OK":
-            result["num_failed"] += 1
-            result["tests_failed"].append("Publish Dataverse")
-            result["Publish Dataverse"] = {"status_code": publish_dataverse.status_code, "text": json_publish_dv["message"]}
-
+        app.logger.debug("Publishing dataset")
         # Publish Dataset
         publish_dataset = requests.post(
             dataverse_endpoint
             + '/api/datasets/:persistentId/actions/:publish?persistentId='
             + persistent_id
-            + '&type=major',
+            + '&type=major'
+            + '&assureIsIndexed=true',
             headers=headers,
             verify=False)
         json_publish_dataset = publish_dataset.json()
+
+        # Wait for changes to take effect
+        while publish_dataset.status_code != 200 and json_publish_dataset["message"] == "Dataset is awaiting indexing":
+            app.logger.debug("Waiting in while loop dataset")
+            time.sleep(3.0)
+            publish_dataset = requests.post(
+                dataverse_endpoint
+                + '/api/datasets/:persistentId/actions/:publish?persistentId='
+                + persistent_id
+                + '&type=major'
+                + '&assureIsIndexed=true',
+                headers=headers,
+                verify=False)
+            json_publish_dataset = publish_dataset.json()
+
         if json_publish_dataset["status"] != "OK":
             result["num_failed"] += 1
             result["tests_failed"].append("Publish Dataset")
-            result["Publish Dataset"] = {"status_code": publish_dataset.status_code, "text": json_publish_dataset["message"]}
+            result["Failed Publish Dataset"] = {"status_code": publish_dataset.status_code, "text": json_publish_dataset["message"]}
+        result["info"]["Publish Dataset"] = {"status_code": publish_dataset.status_code}
 
+        # Another wait for safe measure
+        time.sleep(5.0)
+
+        app.logger.debug("Delete dataset")
         # Delete Published Dataset
         delete_published_ds = requests.delete(
             dataverse_endpoint + '/api/datasets/' + str(dataset_id) + '/destroy',
@@ -206,19 +123,10 @@ def define_resources(app):
         if json_delete_published_ds["status"] != "OK":
             result["num_failed"] += 1
             result["tests_failed"].append("Delete Published Dataset")
-            result["Delete Published Dataset"] = {"status_code": delete_published_ds.status_code, "text": json_delete_published_ds["message"]}
-
-        # Delete Dataverse
-        delete_dataverse = requests.delete(
-            dataverse_endpoint + '/api/dataverses/science',
-            headers=headers,
-            verify=False)
-        json_delete_dv = delete_dataverse.json()
-        if json_delete_dv["status"] != "OK":
-            result["num_failed"] += 1
-            result["tests_failed"].append("Delete Dataverse")
-            result["Delete Dataverse"] = {"status_code": create_dataverse.status_code, "text": json_delete_dv["message"]}
+            result["Failed Delete Published Dataset"] = {"status_code": delete_published_ds.status_code, "text": json_delete_published_ds["message"]}
+        result["info"]["Delete Published Dataset"] = {"status_code": delete_published_ds.status_code}
 
         return json.dumps(result)
 
 # Message listener for transfer-ready queue
+# Check S3 for export - delete after verified
